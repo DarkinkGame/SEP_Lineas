63354d2f1bd7307d76f428fdf099d98a232b947e
Bibi <vale123.bere@gmail.com>
2026-05-25 13:49:19 -0600

    Comentario nuevo

--- CalculadoraLineas.py
+++ CalculadoraLineas.py
@@ -47,8 +47,6 @@ def calcular_regulacion():
         if not nombre_cond:
             messagebox.showerror("Error de selección", "Por favor, selecciona un conductor primero.")
             return
-        #################################### Lineas Medias ##########################################
-        ####### Aquí pongo un comentaario
         if longitud_km > 0 and longitud_km < 80:
             tipo_linea = "Corta: Admitancia en Paralelo Despreciada"
             r_unitaria = float(diccionario_conductores[nombre_cond]["Resistencia"])
